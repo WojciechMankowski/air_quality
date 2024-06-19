6c99728b56ffa44d2941b0cad45a77c5d3b00d16
wojciech <wmankowski@gmail.com>
2024-06-19 09:52:38 +0200

    add coorect in program

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
 import os
+from datetime import datetime
 from time import sleep
-from air_quality_data import get_data
-from Databese import AirQualityDatabase
 from dotenv import load_dotenv
+from Databese import AirQualityDatabase  # Assuming the module and class are imported correctly
+from air_quality_data import get_data  # Assuming the module and function are imported correctly
 
-def main():
+
+def initialize_database():
     load_dotenv()
     user = os.getenv("USER")
-    password =os.getenv('PASSWORD')
+    password = os.getenv("PASSWORD")
     host = os.getenv("HOST")
-    db = AirQualityDatabase(user, password, host=host)
+    return AirQualityDatabase(user, password, host=host)
+
+
+def process_sensor_data(db):
     for ids in db.get_data("Sensors"):
         data = get_data(ids[0])
         key = data["key"]
         values = data["values"]
+        process_values(db, key, values, ids[0])
 
-        if isinstance(values, list):
-            for item in values:
-                data_data = db.get_data('air_quality')
-                # print(data_data)
-                db.add_air_quality_data(len(data_data)+1, key, item["date"], item["value"], ids[0])
+
+def process_values(db, key, values, sensor_id):
+    if isinstance(values, list):
+        for item in values:
+            date_st = item['date'][:10]
+            date_now = datetime.today().date()
+            date_str = date_now.strftime("%Y-%m-%d")
+            is_today = date_str == date_st
+            if is_today:
+                add_air_quality_data(db, key, item, sensor_id)
                 sleep(15)
 
-if __name__ == '__main__':
+
+def add_air_quality_data(db, key, item, sensor_id):
+    data_data = db.get_data('air_quality')
+    db.add_air_quality_data(len(data_data) + 1, key, item["date"], item["value"], sensor_id)
+
+
+def main():
+    db = initialize_database()
+    process_sensor_data(db)
+
+
+if __name__ == "__main__":
     main()
