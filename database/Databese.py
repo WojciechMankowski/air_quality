625cd0b3e750ef667ec55abcd775249e523a1393
wojciech <wmankowski@gmail.com>
2024-06-20 09:55:46 +0200

    add coorect in program

--- database/Databese.py
+++ database/Databese.py
@@ -96,7 +96,7 @@ class AirQualityDatabase:
             # Dane nie istnieją, wykonaj INSERT
             id = self.cursor.execute("SELECT id FROM air_quality")
             data = self.cursor.fetchall()
-            print(data[-1])
+            print(data[-1][0]+1)
             self.cursor.execute(
                 """
                 INSERT INTO air_quality (id, pollutant, measurement_date, value, sensor_id)
