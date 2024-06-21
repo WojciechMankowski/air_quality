b884eef22d7f26d525b324ba00b275c1214fda59
wojciech <wmankowski@gmail.com>
2024-06-21 10:24:16 +0200

    add correct

--- uruchom_skrypty.sh
+++ uruchom_skrypty.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-python3 ./dowland_data_for_city/Cities/bydgoszcz.py  &
-python3 ./dowland_data_for_city/Cities/gdansk.py &
-python3 ./dowland_data_for_city/Cities/katowice.py &
-python3 ./dowland_data_for_city/Cities/krakow.py &
-python3 ./dowland_data_for_city/Cities/lodz.py &
-python3 ./dowland_data_for_city/Cities/lublin.py &
-python3 ./dowland_data_for_city/Cities/poznan.py &
-python3 ./dowland_data_for_city/Cities/szczecin.py &
-python3 ./dowland_data_for_city/Cities/warsaw.py &
-python3 ./dowland_data_for_city/Cities/wroclaw.py &
+python3 ./dowland_data_for_city/bydgoszcz.py  &
+python3 ./dowland_data_for_city/gdansk.py &
+python3 ./dowland_data_for_city/katowice.py &
+python3 ./dowland_data_for_city/krakow.py &
+python3 ./dowland_data_for_city/lodz.py &
+python3 ./dowland_data_for_city/lublin.py &
+python3 ./dowland_data_for_city/poznan.py &
+python3 ./dowland_data_for_city/szczecin.py &
+python3 ./dowland_data_for_city/warsaw.py &
+python3 ./dowland_data_for_city/wroclaw.py &
 wait
